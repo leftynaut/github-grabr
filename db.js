0f3ea57089edefad788512ddea944effea66a562
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2016-10-16 12:39:41 -0500

    config.js broke it v2

--- db.js
+++ db.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const vars = require('./config');
-
 const connection = `mongodb://${process.env.dbHost}/${process.env.dbName}`;
 const mongoose = require('mongoose');
 const db = mongoose.connect(connection);
